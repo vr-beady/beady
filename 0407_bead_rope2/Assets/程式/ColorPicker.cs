4fbe0011bc53cf0eb4c00c6700fc1b4da60b29f5
ajian-tsai <vrbeady@gmail.com>
2023-05-17 16:23:57 +0800

    add_相機轉動_顏色選擇器加入_但座標沒影轉換完成

---
+++ 0407_bead_rope2/Assets/程式/ColorPicker.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorPicker : MonoBehaviour
+{
+    [SerializeField]
+    Camera cam;
+    [SerializeField]
+    RectTransform texture;
+    [SerializeField]
+    RectTransform textureRightUp;
+    [SerializeField]
+    GameObject showColor;
+
+    [SerializeField]
+    Texture2D refSprite;
+
+    public void OnClickPickerColor()
+    {
+        SetColor();
+    }
+    private void SetColor()
+    {
+        Vector3 imagePos = cam.WorldToScreenPoint(texture.position);
+        Vector3 imagePos_RightUp = cam.WorldToScreenPoint(textureRightUp.position);
+        float globalPosX = Input.mousePosition.x - imagePos.x;
+        float globalPosY = Input.mousePosition.y - imagePos.y;
+
+        int localPosX = (int)(globalPosX * (refSprite.width / (imagePos_RightUp.x - imagePos.x)));
+        int localPosY = (int)(globalPosY * (refSprite.height / (imagePos_RightUp.y - imagePos.y)));
+
+        Color c = refSprite.GetPixel(localPosX, localPosY);
+        SetActualColor(c);
+
+        Debug.Log("globalPos:" + globalPosX + "," + globalPosY);
+        Debug.Log("localPos:" + localPosX + "," + localPosY);
+        Debug.Log("imagePos:" + imagePos);
+        Debug.Log("imagePos_RightUp:" + imagePos_RightUp);
+    }
+
+    void SetActualColor(Color c)
+    {
+        showColor.GetComponent<Image>().color = c;
+    }
+}
---
+++ 0407_bead_rope2/Assets/程式/camera_rotate.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_rotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // Start is called before the first frame update
+    private float rotationSpeed = 5;
+    private float moveSpeed = 10;
+    private float updownSpeed = 2;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * horizontalInput * moveSpeed * Time.deltaTime);
+        transform.Translate(Vector3.forward * verticalInput * moveSpeed * Time.deltaTime);
+        if(Input.GetKey("q"))
+        {
+            transform.Translate(Vector3.down * updownSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey("e"))
+        {
+            transform.Translate(Vector3.up * updownSpeed * Time.deltaTime);
+        }
+        /*
+        float mouseXInput = Input.GetAxis("Mouse X");
+        float mouseYInput = Input.GetAxis("Mouse Y");
+        transform.Rotate(Vector3.up, mouseXInput);
+        transform.Rotate(Vector3.left, mouseYInput);
+        */
+    }
+}
